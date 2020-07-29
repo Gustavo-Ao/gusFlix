8e0f7077ceefabb9405974261f4d1278f5bdcc43
Gustavo Antonio Oliveira <gustavo@pop-os.localdomain>
2020-07-28 22:35:30 -0300

    Adicionado telas de Novo Vide e Cadastrar Categoria; Adicionado rotas; Adicionado novo carousel;

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button'
+import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img src={Logo} className="Logo" alt="GusFlix Logo" />
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </nav>
